7311ebee50b66d81edbc73a9f8a65de7db807d40
Alwin <alwin@Alwins-MacBook-Air.local>
2023-11-29 11:13:38 +0530

    removed expired announcements

--- data/announcements.ts
+++ data/announcements.ts
@@ -5,18 +5,10 @@ interface Announcement {
 type AnnouncementsArray = Announcement[];
 
 const announcements: AnnouncementsArray = [
-  {
-    title:
-      "Sample question papers for BS & MTech in DS & AI are available for Download in the Admissions 2023 Sidebar Widget",
-  },
   // {
   //   title:
   //     "Applications deadline is extended till 12th August. Hurry! Applications are closing {{DAYS_LEFT,2023-08-12}}. Apply Now!",
   // },
-  {
-    title:
-      "For MTech applications - Last date for exam registrations to the mandatory NPTEL courses is on 10/10/2023",
-  },
 ];
 
 export default announcements;
