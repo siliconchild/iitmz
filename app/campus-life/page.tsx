0b5be406454d5230830817445638ea0d04dfa23c
Alwin <alwingeorge2008@gmail.com>
2024-03-21 16:11:07 +0530

    fixed image file path

--- app/campus-life/page.tsx
+++ app/campus-life/page.tsx
@@ -77,7 +77,7 @@ export default function CampusLife() {
           {Array.from({ length: 12 }, (_, index) => (
             <Img
               key={`image__${index}`}
-              src={`/campus-life-scroll/${index + 1}.JPG`}
+              src={`/campus-life-images/${index + 1}.jpg`}
               width={450}
               height={350}
               alt="Image of IITM Zanzibar Campus"
