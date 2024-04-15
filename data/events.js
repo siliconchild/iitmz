5c069aa4ad358602927628060f68130e3961bf82
unknown <alwingeorge2008@gmail.com>
2024-04-15 14:12:38 +0530

    added event

--- data/events.js
+++ data/events.js
@@ -1,5 +1,17 @@
 const eventsList = [
   {
+    img: "/webinar.png",
+    date: {
+      day: "16",
+      monthYear: "April 2024",
+    },
+    link: "https://us06web.zoom.us/webinar/register/WN_7AuU0zV4ToaM37eOExj31A",
+    name: "Discover Excellence: BS in Data Science & AI at IITM Zanzibar",
+    time: "03:00 PM EAT | 05:30 PM IST",
+    location: "Virtual",
+    desc: "Join the webinar to get insights from our distinguished speakers on how the BS program at IIT Madras Zanzibar equips students with the skills, knowledge, and global perspective needed to thrive in the digital age. The panel will highlight the innovative & interdisciplinary curriculum, cutting-edge research, industry collaborations with career prospects and global opportunities embedded in the BS in Data Science & AI program at IIT Madras Zanzibar!",
+  },
+  {
     img: "/IITMZ-Webinar 2.png",
     date: {
       day: "16",
@@ -9,7 +21,7 @@ const eventsList = [
     name: "Discover IITM Zanzibar - Africa Edition Special Webinar",
     time: "05:00 PM EAT | 07:30 PM IST",
     location: "Virtual",
-    desc: "This Discover IITM Zanzibar will focus on living & learning at the IITM Zanzibar Campus, especially for BS Students. All African Nationals studying & working in other countries are also welcome to join this webinar! Join the webinar to hear from the 1st cohort of students in BS Program on why you need to choose the IIT Madras Zanzibar Campus for your Undergraduate Studies!"
+    desc: "This Discover IITM Zanzibar will focus on living & learning at the IITM Zanzibar Campus, especially for BS Students. All African Nationals studying & working in other countries are also welcome to join this webinar! Join the webinar to hear from the 1st cohort of students in BS Program on why you need to choose the IIT Madras Zanzibar Campus for your Undergraduate Studies!",
   },
 ];
 
