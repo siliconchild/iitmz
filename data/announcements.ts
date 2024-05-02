9fd551839d91fb84801638d19f3eb0e933b30385
Alwin <alwingeorge2008@gmail.com>
2024-05-02 12:04:13 +0530

    removed outdated announcement

--- data/announcements.ts
+++ data/announcements.ts
@@ -12,9 +12,6 @@ const announcements: AnnouncementsArray = [
     title:
       "Application Period Extended for BS in Data Science & AI: Last Date to Apply: May 05, 2024 | 11:59 PM IST",
   },
-  {
-    title: "MTech Application closing {{DAYS_LEFT,2024-03-18}}",
-  },
 ];
 
 export default announcements;
