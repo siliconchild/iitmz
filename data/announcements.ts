98225ca7ce6d63093066e07da4cc550b1d518ca2
unknown <alwingeorge2008@gmail.com>
2024-06-24 12:50:13 +0530

    updated announcement

--- data/announcements.ts
+++ data/announcements.ts
@@ -12,6 +12,9 @@ const announcements: AnnouncementsArray = [
     title:
       "BS selection process is underway. Candidates are requested to check their registered email for next steps",
   },
+	{
+		title: "MTech Data Science & AI - Application Deadline has been extended to July 05, 2024",
+	},
   {
     title:
       "MTech Ocean Structures - Application Deadline has been extended to June 30, 2024",
