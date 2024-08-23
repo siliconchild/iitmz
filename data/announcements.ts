fe1c820d450b96395b4c864dc34c019cbf96b8e7
Pavithra <cmsdev_1@ge.iitm.ac.in>
2024-08-23 12:43:43 +0530

    Removed outdated items and added new details in announcements

--- data/announcements.ts
+++ data/announcements.ts
@@ -8,17 +8,25 @@ const announcements: AnnouncementsArray = [
   // {
   //   title: "BS Application closing {{DAYS_LEFT,2024-04-30}}",
   // },
+  // {
+  //   title:
+  //     "Selected BS & MTech candidates are requested to email 'admissions@iitmz.ac.in' for any queries",
+  // },
+  // {
+  //   title:
+  //     "MTech Ocean Structures & MTech Data Science & AI- Candidates are requested to check their emails for admission status and further steps",
+  // },
+  // {
+  //   title:
+  //     "MTech Data Science & AI - Application Deadline has been extended to July 20, 2024",
+  // },
   {
     title:
-      "Selected BS & MTech candidates are requested to email 'admissions@iitmz.ac.in' for any queries",
+      "Applications are live for the MTech Program in Ocean Structures from candidates of all Nationalities. Applications will close by Sep 15, 2024",
   },
   {
     title:
-      "MTech Ocean Structures & MTech Data Science & AI- Candidates are requested to check their emails for admission status and further steps",
-  },
-  {
-    title:
-      "MTech Data Science & AI - Application Deadline has been extended to July 20, 2024",
+      "Selected BS & MTech candidates are requested to check their emails for updates from the admissions team",
   },
 ];
 
