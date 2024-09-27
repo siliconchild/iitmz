1339b3f2217a29d09d1525a89006e82da42938e5
unknown <alwingeorge2008@gmail.com>
2024-09-27 10:59:05 +0530

    added new calendar

--- data/calendar.ts
+++ data/calendar.ts
@@ -1,6 +1,10 @@
 export const academicCalendarData = [
   {
-    title: "March - July 2024",
+    title: "March 2024 - July 2024",
     link: "/AcademicCalendarMar-July2024.pdf",
+  },
+  {
+    title: "Oct 2024 - Feb 2025",
+    link: "/AcademicCalendar-October-2024-February-2025.pdf",
   },
 ];
