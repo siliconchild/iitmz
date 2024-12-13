968de35bed0fc0580e3fc7d5f237a115209f78c4
unknown <alwingeorge2008@gmail.com>
2024-12-13 14:39:09 +0530

    updated ticker, banner and added html support

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 import ApplyDropdown from "./apply-dropdown";
 
 export default function Hero() {
-
   return (
     <section>
       <Slider autoplayInterval={8000}>
@@ -22,9 +21,9 @@ export default function Hero() {
 
           <div className={styles.content}>
             {/* <h4>IITM Zanzibar Campus</h4> */}
-            <h3>Welcome To</h3>
+            <h3>Web-Enabled Masters Program for working professionals</h3>
             <h1>
-              IIT Madras <span>Zanzibar</span>
+              MTech <span>Industrial AI</span>
             </h1>
             {/* <h4>
               MTech Ocean Structures Applications are live
@@ -32,11 +31,12 @@ export default function Hero() {
             <h5>
             Last Date: Sep 15, 2024 | 11:59 PM
             </h5>
+            */}
             <Button>
-              <Link target="_blank" href="https://admissions.ge.iitm.ac.in/iitmz-mtechos">
+              <Link target="_blank" href="https://admissions.iitmz.ac.in/iitmz-mtechiai">
                 Apply Now
               </Link>
-            </Button> */}
+            </Button>
           </div>
         </div>
         <div className={styles.item}>
--- data/announcements.ts
+++ data/announcements.ts
@@ -8,25 +8,12 @@ const announcements: AnnouncementsArray = [
   // {
   //   title: "BS Application closing {{DAYS_LEFT,2024-04-30}}",
   // },
-  // {
-  //   title:
-  //     "Selected BS & MTech candidates are requested to email 'admissions@iitmz.ac.in' for any queries",
-  // },
-  // {
-  //   title:
-  //     "MTech Ocean Structures & MTech Data Science & AI- Candidates are requested to check their emails for admission status and further steps",
-  // },
-  // {
-  //   title:
-  //     "MTech Data Science & AI - Application Deadline has been extended to July 20, 2024",
-  // },
   {
-    title:
-      "BS & MTech classes have started from Oct' 24",
+    title: "Admissions for the AY 2025-26: BS & MTech full-time degree programs will start from 1 Feb 2024",
   },
   {
     title:
-      "Admissions for the AY 2025-26: BS & MTech programs will start from 1 Feb 2024",
+      "MTech Industrial AI (Web-Enabled) Application Form is Live - <a href='https://admissions.iitmz.ac.in/iitmz-mtechiai' target='_blank'><strong>&nbsp;Apply Now</strong></a>",
   },
 ];
 
