e0da9545e43d21b64b9fa86bca6201f0bd093409
Pavithra <cmsdev_1@ge.iitm.ac.in>
2025-01-21 11:11:12 +0530

    removed existing and added new items

--- data/announcements.ts
+++ data/announcements.ts
@@ -9,13 +9,14 @@ const announcements: AnnouncementsArray = [
   //   title: "BS Application closing {{DAYS_LEFT,2024-04-30}}",
   // },
   {
-    title:
-      "Admissions for the AY 2025-26: BS & MTech full-time degree programs will start from 1 Feb 2025 - <a href='https://admissions.iitmz.ac.in/' target='_blank'><strong>&nbsp;Apply Now</strong></a>",
+    title: "Application Duration for the BS & MTech programs: Jan 10 - May 31, 2025 -  <a href='https://admissions.iitmz.ac.in/' target='_blank'><strong>&nbsp;Click here</strong></a> to know more"
   },
   {
-    title:
-      "Application Duration for the BS & MTech programs - Feb 01 - May 31, 2025 -  <a href='https://admissions.iitmz.ac.in/' target='_blank'><strong>&nbsp;Click here</strong></a> to know more",
+    title: "Admissions for the AY 2025-26: BS & MTech full-time degree programs starts from Jan 10, 2025 -  <a href='https://admissions.iitmz.ac.in/' target='_blank'><strong>&nbsp;Apply Now</strong></a>"
   },
+  {
+    title: "More details on the programs offered and complete admission process -  <a href='https://www.iitmz.ac.in/admission' target='_blank'><strong>&nbsp;Click here</strong></a>"
+  }
 ];
 
 export default announcements;
