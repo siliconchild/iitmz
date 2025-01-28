7963d57d4f8f2c3db07ea8d0f684892ec75c400c
Pavithra <cmsdev_1@ge.iitm.ac.in>
2025-01-28 14:31:06 +0530

    added new sections

--- components/course-eligibility.tsx
+++ components/course-eligibility.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import styles from "./course-eligibility.module.scss";
 
-const CourseEligibility: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const CourseEligibility: React.FC<{ children: React.ReactNode[] }> = ({ children }) => {
   return (
     <section className={styles.section}>
       <h3>Eligibility Criteria</h3>
       <div className={styles.panel}>
-        <ul>{children}</ul>
+        {children}
       </div>
     </section>
   );
