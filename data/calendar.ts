c0bb562eca41decf00347ac82357f55afedc97e9
Pavithra <alwingeorge2008@gmail.com>
2025-09-23 10:51:42 +0530

    updated award image

--- data/calendar.ts
+++ data/calendar.ts
@@ -8,7 +8,7 @@ export const academicCalendarData = [
     link: "/Odd-Semester_ All-others-(Oct'25-Feb'26)_updated.pdf",
   },
   {
-    title: "Even Semester (Mar 2026 – Jul 2026)",
+    title: "Even Semester: (Mar 2026 – Jul 2026)",
     link: "/Even-Semester-(Mar'25-Jul'26)_updated.pdf",
   },
   {
