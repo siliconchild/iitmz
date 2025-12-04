3e86ad78381b9b495241fbd9ef52970fe713c86d
Pavithra <alwingeorge2008@gmail.com>
2025-12-04 12:42:53 +0530

    added mtech ordinance

--- data/ordinances.ts
+++ data/ordinances.ts
@@ -3,4 +3,8 @@ export const academicOrdinancesData = [
     title: "BS Ordinance",
     link: "/bs-ordinances.pdf",
   },
+  {
+    title: "MTech Ordinance",
+    link: "/MTechZanzibarOrdiance.pdf",
+  },
 ];
